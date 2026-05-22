1314567105fe87b4c591e90f2f597c8966a3e6fa
JulSergeeva <sergeevajulija@bk.ru>
2026-05-22 13:28:04 +0200

    add faker

--- pythonProject/api_client_create_exercise.py
+++ pythonProject/api_client_create_exercise.py
@@ -1,17 +1,18 @@
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
-from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict
+from clients.courses.courses_client import get_courses_client
+from clients.courses.courses_schema import CreateCourseRequestSchema
+from clients.exercises.exercises_client import get_exercises_client
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema
 from clients.files.files_client import get_files_client
 from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import get_random_email
-
+from tools.fakers import fake
 
 public_user_client = get_public_users_client()
 
 create_user_request = CreateUserRequestSchema(
-    email= get_random_email(),
+    email= fake.email(),
     password="string",
     last_name="string",
     first_name="string",
@@ -38,7 +39,7 @@ create_file_request = CreateFileRequestSchema(
 create_file_response = files_client.create_file(create_file_request)
 print(f"Create file data: {create_file_response}")
 
-create_course_request = CreateCourseRequestDict(
+create_course_request = CreateCourseRequestSchema(
     title="Python",
     maxScore=100,
     minScore=10,
@@ -51,9 +52,9 @@ create_course_request = CreateCourseRequestDict(
 create_course_response = course_client.create_course(create_course_request)
 print(f"Create course data: {create_course_response}")
 
-create_exercise_request = CreateExerciseRequestDict(
+create_exercise_request = CreateExerciseRequestSchema(
     title="Exercise 1",
-    courseId=create_course_response["course"]["id"],
+    courseId=create_course_response.course.id,
     maxScore=5,
     minScore=1,
     orderIndex= 0,
